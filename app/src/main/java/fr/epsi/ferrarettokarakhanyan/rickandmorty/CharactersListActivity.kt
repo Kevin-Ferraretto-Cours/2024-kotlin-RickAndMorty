c3d37749756ba0a5c7b5cfc47204ed841013a33a
MrSuricate <kevindu032270@gmail.com>
2024-10-24 14:28:00 +0200

    rename MainActivity to CharactersListActivity

--- app/src/main/java/fr/epsi/ferrarettokarakhanyan/rickandmorty/CharactersListActivity.kt
+++ app/src/main/java/fr/epsi/ferrarettokarakhanyan/rickandmorty/CharactersListActivity.kt
@@ -13,7 +13,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import fr.epsi.ferrarettokarakhanyan.rickandmorty.ui.theme.RickAndMortyTheme
 
-class MainActivity : ComponentActivity() {
+class CharactersListActivity : ComponentActivity() {
 	override fun onCreate(savedInstanceState : Bundle?) {
 		super.onCreate(savedInstanceState)
 		enableEdgeToEdge()
