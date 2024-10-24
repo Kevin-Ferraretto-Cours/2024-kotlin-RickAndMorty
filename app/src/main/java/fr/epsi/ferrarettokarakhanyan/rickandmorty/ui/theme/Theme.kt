e7166d9f21a60fbd2c035d300cd22feefdfeb81b
MrSuricate <kevindu032270@gmail.com>
2024-10-24 14:45:53 +0200

    add color purple

--- app/src/main/java/fr/epsi/ferrarettokarakhanyan/rickandmorty/ui/theme/Theme.kt
+++ app/src/main/java/fr/epsi/ferrarettokarakhanyan/rickandmorty/ui/theme/Theme.kt
@@ -11,11 +11,11 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.platform.LocalContext
 
 private val DarkColorScheme = darkColorScheme(
-	primary = Purple80,secondary = PurpleGrey80,tertiary = Pink80
+	primary = Purple,secondary = PurpleGrey80,tertiary = Pink80
 )
 
 private val LightColorScheme = lightColorScheme(
-	primary = Purple40,secondary = PurpleGrey40,tertiary = Pink40
+	primary = Purple,secondary = PurpleGrey40,tertiary = Pink40
 
 	/* Other default colors to override
     background = Color(0xFFFFFBFE),
